507c1a0d6f8e44522b46ff18f12b677d7fbdeeb7
soham <sohamcool411@gmail.com>
2024-09-14 23:58:24 +0530

    removed email section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
