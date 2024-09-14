9be3c06a62a434d092a8c2d4ede4c5c82edd669d
soham <sohamcool411@gmail.com>
2024-09-14 23:35:46 +0530

    new commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,12 +56,12 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/Sohamcool411">
+          <a target='_blank' href="https://github.com/Sohamcool411">
             <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
-          <Link href="https://linkedin.com/in/soham-patil21">
+          </a>
+          <a target='_blank' href="https://linkedin.com/in/soham-patil21">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
+          </a>
         </div>
       </div>
       <div>
